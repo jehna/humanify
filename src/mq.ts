b94501e82dd9e1fbd0815b7f8ce1b40cfae09331
raph-son <samezzy@yahoo.com>
2023-12-12 11:30:20 +0100

    Fixed socket communication for windows

--- src/mq.ts
+++ src/mq.ts
@@ -1,9 +1,16 @@
+import os from 'os';
+
 import zmq from "zeromq";
 import { spawn } from "child_process";
 
 const sock = zmq.socket("req");
 
-sock.connect("ipc:///tmp/humanify-local-inference-server.ipc");
+// Allow communication support for windows
+if (os.platform() === 'win32') {
+  sock.connect("inproc:///tmp/humanify-local-inference-server.ipc");
+} else {
+  sock.connect("ipc:///tmp/humanify-local-inference-server.ipc");
+}
 
 export function send<Recv extends {}>(message: Object) {
   sock.send(JSON.stringify(message));
