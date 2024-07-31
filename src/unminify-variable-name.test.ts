070370efaaed4885ad96855373d9bcd54228fc98
Jesse Luoto <jesse.luoto@gmail.com>
2024-07-31 22:40:38 +0300

    Change test setup to a simpler one
    Better to have one that is reproducable at the dev time. I'd rateher
    have npm scripts dictating everything than try to bury it to yaml files.

--- src/unminify-variable-name.test.ts
+++ src/unminify-variable-name.test.ts
@@ -3,7 +3,7 @@ import { llama } from "./llama.js";
 import { unminifyVariableName } from "./unminify-variable-name.js";
 import { assertMatches } from "./test-utils.js";
 
-const prompt = await llama({ seed: 1 }); // TODO: Figure out how to download a small enough model
+const prompt = await llama({ seed: 1, modelPath: process.env["MODEL"] }); // TODO: Figure out how to download a small enough model
 
 test("Unminifies a function name", async (t) => {
   const result = await unminifyVariableName(
@@ -22,5 +22,5 @@ test("Unminifies an argument", async (t) => {
     "math-utils.js",
     "const addOne = b => b + 1;"
   );
-  assertMatches(result, ["num", "number", "val", "value"]);
+  assertMatches(result, ["num", "number", "val", "value", "accumulator"]);
 });
