f78bc7f9060d84adc43a73e1e3ed5ce5e9d6fdd0
Jesse Luoto <jesse.luoto@gmail.com>
2024-08-09 19:34:02 +0300

    Add OpenAI e2e tests

--- src/commands/openai.ts
+++ src/commands/openai.ts
@@ -4,13 +4,17 @@ import { unminify } from "../unminify.js";
 import babel from "../plugins/babel/babel.js";
 import { openaiRename } from "../plugins/openai/openai-rename.js";
 import { verbose } from "../verbose.js";
+import { env } from "../env.js";
 
 export const openai = cli()
   .name("openai")
   .description("Use OpenAI's API to unminify code")
   .option("-m, --model <model>", "The model to use", "gpt-4o-mini")
   .option("-o, --outputDir <output>", "The output directory", "output")
-  .option("-k, --apiKey <apiKey>", "The OpenAI API key")
+  .option(
+    "-k, --apiKey <apiKey>",
+    "The OpenAI API key. Alternatively use OPENAI_API_KEY environment variable"
+  )
   .option("--verbose", "Show verbose output")
   .argument("input", "The input minified Javascript file")
   .action(async (filename, opts) => {
@@ -18,7 +22,7 @@ export const openai = cli()
       verbose.enabled = true;
     }
 
-    const apiKey = opts.apiKey ?? process.env.OPENAI_API_KEY;
+    const apiKey = opts.apiKey ?? env("OPENAI_API_KEY");
     await unminify(filename, opts.outputDir, [
       babel,
       openaiRename({ apiKey, model: opts.model }),
